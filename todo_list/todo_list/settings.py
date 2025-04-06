c1231cb15ffac4bdd8d45b9f829e821d8191ab88
Mistire <mdwithgod@gmail.com>
2025-04-07 01:37:33 +0300

    Fix issues

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-79blsb5__io#qehoxq4nyh1rqqq@76ao+p-p-l=ib6vxsqhog+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['todo-list-apii.netlify.app', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -125,12 +125,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
